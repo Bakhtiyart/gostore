eb93349f28643625c3e6260125e980f88d432b90
bakhtiyar <bakhtiyarkhozha@gmail.com>
2020-01-17 17:35:51 +0500

    update footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
     
-    
+    $('#menu .hamburger').click(function(){
+        if($(this).hasClass('is-active')){
+            $(this).removeClass('is-active');
+        }
+        else{
+            $(this).addClass('is-active');
+        }
+    });
 });
